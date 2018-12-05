71a7dab0ea4587b509633e96d362c892aa31480a
Apilav1 <apilav1@etf.unsa.ba>
2018-12-05 22:39:26 +0100

    Zavrsen zadatak 1

--- src/ba/unsa/rpr/tutorijal7/Tutorijal.java
+++ src/ba/unsa/rpr/tutorijal7/Tutorijal.java
@@ -1,7 +1,45 @@
 package ba.unsa.rpr.tutorijal7;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Tutorijal {
+    public static ArrayList<Grad> ucitajGradove(){
+        ArrayList<Grad> gradovi = new ArrayList<>();
+        Scanner ulaz;
+        try{
+            ulaz = new Scanner(new FileReader("mjerenja.txt"));
+            ulaz.useDelimiter("\n");
+        }
+        catch (FileNotFoundException e){
+            System.out.println("Datoteka ne postoji, greska: " + e);
+            return null;
+        }
+        try{
+            //Sarajevo,5.4,15.8,2.3,0.44,-1.1
+            //Banja Luka,8.7,20.1,5.6,4.3,2.2,1.1,-2.2,-8.777
+            while(ulaz.hasNext()){
+                Grad novi_grad = new Grad();
+                double[] nove_temperature = new double[1000];
+                String[] nova_linija = ulaz.nextLine().split(",");
+                novi_grad.setNaziv(nova_linija[0]);
+                for(int i=1; i < nova_linija.length; i++) {
+                    if(i == 1000) break;
+                    nove_temperature[i-1] = Double.parseDouble(nova_linija[i]);
+                }
+                novi_grad.setTemperature(nove_temperature);
+                gradovi.add(novi_grad);
+            }
+        }
+        catch (Exception e){
+            System.out.println("Greska pri citanju podataka, " + e);
+        }
+        return null;
+    }
     public static void main(String[] args) {
         // write your code here
+        ucitajGradove();
     }
 }
